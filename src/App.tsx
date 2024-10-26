6e92e2b2e59dd31c9bd3aa66acfda99ded282e75
Susie Douang <soutsada.d@gmail.com>
2024-10-26 16:28:53 -0500

    add axios library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider, Box, Button } from '@chakra-ui/react'
+import axios from 'axios';
+
 
 function App() {
-
+const handleClick  = async () => {
+  const response = axios.get('http://localhost:5001');
+  console.log("Response", response);
+}
   return (
     <ChakraProvider>
       <Box>Hi</Box>
-      <Button colorScheme='purple'>Button</Button>
+      <Button colorScheme='purple' onClick={handleClick}>Button</Button>
     </ChakraProvider>
   )
 }
